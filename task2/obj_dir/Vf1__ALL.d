6afa9ba9bec8b88a9365c7d3a71438735e5764b2
anish-narain <69715492+anish-narain@users.noreply.github.com>
2022-11-04 00:49:02 +0000

    task 2

---
+++ task2/obj_dir/Vf1__ALL.d
@@ -0,0 +1,13 @@
+Vf1__ALL.o: Vf1__ALL.cpp Vf1.cpp Vf1.h \
+  /opt/homebrew/Cellar/verilator/4.228/share/verilator/include/verilated.h \
+  /opt/homebrew/Cellar/verilator/4.228/share/verilator/include/verilatedos.h \
+  /opt/homebrew/Cellar/verilator/4.228/share/verilator/include/verilated_config.h \
+  /opt/homebrew/Cellar/verilator/4.228/share/verilator/include/verilated_types.h \
+  /opt/homebrew/Cellar/verilator/4.228/share/verilator/include/verilated_funcs.h \
+  Vf1__Syms.h Vf1___024root.h \
+  /opt/homebrew/Cellar/verilator/4.228/share/verilator/include/verilated_vcd_c.h \
+  /opt/homebrew/Cellar/verilator/4.228/share/verilator/include/verilated_trace.h \
+  /opt/homebrew/Cellar/verilator/4.228/share/verilator/include/verilated_trace_defs.h \
+  Vf1___024root__DepSet_hce310dbf__0.cpp Vf1__Trace__0.cpp \
+  Vf1___024root__Slow.cpp Vf1___024root__DepSet_hce310dbf__0__Slow.cpp \
+  Vf1__Syms.cpp Vf1__Trace__0__Slow.cpp
